85d67a7ae372ed8bb79290369ec8c66fa86e93c4
Nettles <80995666+nettlees@users.noreply.github.com>
2021-06-01 11:26:28 +0100

    Create chat.js

---
+++ commands/chat.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    name: 'talk', // The name of the command
+    description: 'Random phrases', // The description of the command (for help text)
+    args: false, // Specified that this command doesn't need any data other than the command
+    usage: '', // Help text to explain how to use the command (if it had any arguments)
+    execute(message, args) {
+
+        // List of phrases to respond with
+        var phrases = [
+            'Hello, person.',
+            'Hello friend, i\'m from mars what about you?',
+            'How are you?',
+            'Ugh, let me sleep',
+            'What you up to.'
+        ];
+
+        message.reply(phrases[Math.floor(Math.random()*phrases.length)]);//replies to their message using math to randomise the response sent
+        },
+};
