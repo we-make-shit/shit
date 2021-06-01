40ee440a24ccd60487c12873c44468120976b328
Nettles <80995666+nettlees@users.noreply.github.com>
2021-06-01 11:25:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
+const fs = require('fs');                               // 
+const Discord = require('discord.js');                  // 
+const { prefix, token } = require('./config.json');     // 
+
+const client = new Discord.Client(); 
+client.commands = new Discord.Collection(); 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); 
+
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+// Starts the bot and makes it begin listening for commands.
+client.on('ready', () => {
+    console.log('Bot Online');
+});
+
+
+client.on('message', message => {
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    // If the command isn't in the  command folder, move on
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if(!command) return;
+
+        // If the command requires arguments, make sure they're there.
+        if (command.args && !args.length) {
+            let reply = 'That command requires more details!';
+
+
+            if (command.usage) {
+                reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+            }
+
+            // Send a reply from the bot about any error encountered
+            return message.channel.send(reply);
+        }
+
+
+    if(!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3 ) * 1000;
+
+    if(!timestamps.has(message.author.id)) {
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    } else {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if(now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`Whoa! You're sending commands too fast! Please wait ${timeLeft.toFixed(1)} more second(s) before running \`${command.name}\` again!`);
+        }
+
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+    }
+
+
+    try {
+        // Run the command
+        command.execute(message, args);
+    } catch(error) {
+        console.error(error);
+        message.reply('Sorry! I ran into an error trying to do that!');
+    }
+
+});
+
+client.login(token); // logs into the bot
 
